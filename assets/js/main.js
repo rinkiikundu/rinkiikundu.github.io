0a4edb5f7aef0ab44f36dfd11a60c8b9a55a17d3
Rinki Kundu <114035567+rinkiikundu@users.noreply.github.com>
2023-09-10 14:26:37 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,7 +211,7 @@ themeButton.addEventListener("click", () => {
 
 // Typing Animation using Typed JS
 var typed = new Typed(".type", {
-  strings: ["a Web", "an Android", "a Blockchain"],
+  strings: ["a Full Stack Engineer", "a Network Engineer ", "a UI/UX Developer"],
   smartBackspace: true,
   startDelay: 1000,
   typeSpeed: 130,
